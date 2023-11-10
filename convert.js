b0e750098744f322b93823e4644169d82c6dae3a
Bayarkhuu <bayarkhuu@gmail.com>
2023-11-10 16:57:49 +0800

    helo

---
+++ convert.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("convert");
+
+const mon = input.target.value.toLowerCase();
+const mn = "фцужэнгшүзйыбөахролячёсмит";
+const en = "qwertyuiopasdfghjklzxcvbnm";
+
+let result = "";
+
+mon.split("").forEach((letter, index) => {
+    const letterIndex = mn.indexOf(letter);
+    if (letterIndex !== -1) {
+        result += en[letterIndex];
+    } else {
+        result += letter;
+    }
+});
+
+alert(result);
